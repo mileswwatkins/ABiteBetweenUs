2031f93f7b10cef11055e4b307547ab71209f655
Miles Watkins <mileswwatkins@gmail.com>
2014-06-23 23:51:01 -0400

    Added bounds to isochrone

--- geography_testing/isochrones.js
+++ geography_testing/isochrones.js
@@ -7,13 +7,13 @@ var LATITUDE_DEGREE_LENGTH = 69;
 var LONGITUDE_DEGREE_LENGTH = 53;
 
 // Approxiate mile-per-hour speeds of different transport modes
-var TRANSPORT_MODES = ["walk", "bike", "transit", "drive"]
+var TRANSPORT_MODES = ["walk", "bike", "transit", "drive"];
 var SPEEDS = {
         "walk": 3,
         "bike": 12,
         "transit": 8,
         "drive": 30
-}
+};
 
 // Function to create isochrones based on transport mode, travel time,
 // and starting location
@@ -23,13 +23,25 @@ function createIsochrone(startingLocation, travelTimeInMinutes, transportMode) {
     }
 
     var distance = SPEEDS.transportMode / (travelTime / 60);
-    var degreesEastWest = distance * (1 / LONGITUDE_DEGREE_LENGTH);
-    var degreesNorthSouth = distance * (1 / LATITUDE_DEGREE_LENGTH);
-    var degreesDiagonal = distance * Math.sin(45 * (Math.PI / 180)) * (1 / ((LONGITUDE_DEGREE_LENGTH + LATITUDE_DEGREE_LENGTH) / 2));
+    var deltaNorthSouth = distance * (1 / LATITUDE_DEGREE_LENGTH);
+    var deltaEastWest = distance * (1 / LONGITUDE_DEGREE_LENGTH);
+    var deltaDiagonal = distance * Math.sin(45 * (Math.PI / 180)) * (1 / ((LONGITUDE_DEGREE_LENGTH + LATITUDE_DEGREE_LENGTH) / 2));
 
-    var isochroneBounds = []
+    var startingLat = startingLocation[0]
+    var startingLon = startingLocation[1]
 
-    var isochrone = GeoJSON.parse(isochroneBounds, {"Polygon": })
+    var isochroneBounds = [
+            [startingLat + deltaNorthSouth, startingLon],
+            [startingLat + deltaDiagonal, startingLon + deltaDiagonal],
+            [startingLat, startingLon + deltaEastWest],
+            [startingLat - deltaDiagonal, startingLon + deltaDiagonal],
+            [startingLat - deltaNorthSouth, startingLon],
+            [startingLat - deltaDiagonal, startingLon - deltaDiagonal],
+            [startingLat, startingLon - deltaEastWest],
+            [startingLat + deltaDiagonal, startingLon - deltaDiagonal]
+    ];
+
+    var isochrone = GeoJSON.parse(isochroneBounds, {"Polygon": });
 
     return isochrone;
 }
