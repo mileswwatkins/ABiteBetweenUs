db0754fe6a30daa30912d28d7693f66e389de64c
Scott <sagreine@gmail.com>
2014-07-14 19:05:28 -0400

    adding some test-y requires code

--- web.js
+++ web.js
@@ -1,9 +1,44 @@
 // web.js
 var express = require("express");
 var logfmt = require("logfmt");
+var path = require('path');
+var favicon = require('static-favicon');
+//var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// this first section was (part of) how to hook up to local mongodb
+/*var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/tutorial3data');
+
+// see this link for extremely brief tutorial on setting up mongodb with heroku/node - this may not use monk
+// https://devcenter.heroku.com/articles/getting-started-with-nodejs#using-mongodb
+var mongo = require('mongodb');
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
+*/
+
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
 
 app.use(logfmt.requestLogger());
+
+//app.use('/', routes);
+//app.use('/users', users);
+
 
 app.get('/', function(req, res) {
   res.send('Hello World!');
