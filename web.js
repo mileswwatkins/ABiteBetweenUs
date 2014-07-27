a8ef636eb20eaca48c868a4e170d8d74e59baada
Scott <sagreine@gmail.com>
2014-07-27 19:14:37 -0400

    hey look, some comments. Neat

--- web.js
+++ web.js
@@ -37,24 +37,28 @@ app.use(logfmt.requestLogger());
 //app.use('/', routes);
 //app.use('/users', users);
 
-
+// default page to render at  basic url
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
 
 
-// test, to display google map html
+// Set view engine to html files in the /views/ directory. Path.join makes
+// it environment independent ('/' vs '\')
 app.set('views', path.join(__dirname, '/views'));
-console.log(__dirname);
 
-app.engine('.html', require('ejs').renderFile);
+// Not sure why/how etc. but 'ejs' is what lets you use html files
+app.engine('html', require('ejs').renderFile);
+
+// this was for local running, with jade files. Not needed using ember and HTML
 //app.set('view engine', 'jade');
 
-
+// navigate to {default url}/basic_google_map and render basic_google_map.html
 app.get('/basic_google_map', function(req, res) {
   res.render('basic_google_map.html');
 });
 
+// This is for heroku, don't change the port
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
